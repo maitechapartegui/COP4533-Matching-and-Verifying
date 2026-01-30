3b4430a880d94acab8b39b7c2308ee24748302cd
heirynhr <148991328+heirynhr@users.noreply.github.com>
2026-01-29 20:38:45 -0500

    fixed the objs, initialized the classes in main, started the algorithm

--- src/hospitalStudents.cpp
+++ src/hospitalStudents.cpp
@@ -4,27 +4,38 @@
 using namespace std;
 
 struct Hospital {
-    bool isMatched;
+    int hosNum;
+    // always starts off as false
+    bool isMatched = false;
     // should we make this hold the objects or just match the names?
     vector<int> preferenceList;
-    int matchedStudent;
+    int matchedStudent =0;
     // so basically if they got their first choice second choice etc so we dont have to go throught their preferenec list again 
-    int mathedStudentIndex;
+    int matchedStudentIndex = 1;
+
+    void gotAMatch(int student, int studentIndex){
+        matchedStudent=0;
+        matchedStudentIndex = studentIndex;
+    }
 };
 
 struct Student {
-    bool isMatched;
-    int currentMatchedHos;
-    vector<int> preferenceList;
-    map<int, int> studentOrder;
+    int studentNum;
+    bool isMatched = false;
+    int currentMatchedHos= 0;
+    // basically the index hospital and then the value will be the ranking
+    // prefList[HOSPITAL1] = 1 AKA rank 
+    vector<int> hospitalOrder;
 
-    map<int, int> grabOrder() {
-        //for every thing in the pref list
-        for (int i=1; i < preferenceList.size()+1; i++){
-            //grab the student it is and its i and add it to the map
-            // {hospital, ranking}
-            studentOrder.insert({preferenceList[i],i});
+    bool betterMatch (int currHospital){
+        if(hospitalOrder[currHospital] < hospitalOrder[currentMatchedHos]){
+            // if it is a better match return true
+            return true;
+            // need to edit the new match details in the main
         }
-    };
+        else{
+            return false;
+        }
+    }
     
 };
